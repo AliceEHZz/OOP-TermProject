e3c3af112a0f6d80dd94cf083c1751abe75630eb
Devynn <personaldevynn@gmail.com>
2023-03-07 23:21:21 -0800

    add types

--- src/middleware/passport.middlewares.ts
+++ src/middleware/passport.middlewares.ts
@@ -1,8 +1,9 @@
 import { MockAuthenticationService } from "../areas/authentication/services";
 import passport from "passport";
 import PassportConfig from "../areas/authentication/config/PassportConfig";
+import { Application } from "express";
 
-module.exports = (app) => {
+module.exports = (app: Application) => {
   app.use(passport.initialize());
   app.use(passport.session());
   new PassportConfig(new MockAuthenticationService());
